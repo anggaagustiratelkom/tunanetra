ce42f6d7bb63f4399d47539b204ca8612e00a79b
muzakkialfarisi <muzakkialfarisi10071999@gmail.com>
2022-01-16 01:05:24 +0700

    header

--- application/views/body/dashboard.php
+++ application/views/body/dashboard.php
@@ -1,1 +1,5 @@
-<a type="button" href="<?= base_url('Welcome/v2/')?>" class="btn btn-primary">Primary</a>
+<div class="header text-center py-5">
+    <h3>Aplikasi Edukasi Identifikasi</h3> 
+</div>
+
+<a type="button" href="<?= site_url('Welcome/v2')?>" class="btn btn-light">Mteri Pembelajaran</a>
